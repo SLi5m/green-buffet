520ea47b30e19cbb7192d3577db8aa23a09d5452
sli5m <rogersbag@gmail.com>
2022-03-29 15:34:44 +0300

    Added initial material UI for better grid view

--- src/components/products/MenuCard.js
+++ src/components/products/MenuCard.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './MenuList.css';
-// PushSubscriptionOptions
+
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+
+
 export function  MenuCard ({item, click, buttonText}) {
 
     const handleClick =()=>{
--- src/components/products/MenuList.js
+++ src/components/products/MenuList.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import './MenuList.css';
 import { MenuCard } from './MenuCard.js';
+//syles 
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export function  MenuList ( {items, addToCart}) {
 
     return (
     <div>
-        {items.map((item, itemIndex) => (
-            <div key={itemIndex}>
-                <MenuCard item={item} click={addToCart} buttonText={"Add To Cart"}/>
-            </div>
-        )
-        )}
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+            {items.map((item, itemIndex) => (       
+                <Grid item xs={4}>  <Item key={itemIndex}>
+                        <MenuCard item={item} click={addToCart} buttonText={"Add To Cart"}/>
+                    </Item>
+                </Grid>))}
+            </Grid>
+        </Box>
     </div>)
 }
 
